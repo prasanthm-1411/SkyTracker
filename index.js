3acb7764ebc16b43c1708e02736ffb6d379c468c
PRASANTH M <prasanthm1411@gmail.com>
2024-12-08 13:51:53 +0530

    index.js

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 
-        <script>
-            const apikey="f76b4c3c105a0623d87a666913c52317";
-            const apiurl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
-            const searchbox=document.querySelector(".search input");
-            
-            const searchbtn=document.querySelector(".search button");
-            const weathericon=document.querySelector(".weather-icon");
-            async function checkWeather(city){
-               
-                
-                const response=await fetch(apiurl +city + `&appid=${apikey}`);
-                if(response.status== 404){
-                    document.querySelector(".error").style.display="block";
-                    document.querySelector(".weather").style.display="none";
+const apikey="f76b4c3c105a0623d87a666913c52317";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchbox=document.querySelector(".search input");
 
-                }
-                else{
-                var data=await response.json();
-                console.log(data);
-        
-            document.querySelector(".city").innerHTML= data.name;
-            document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+"°C";
-            document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
-            document.querySelector(".wind").innerHTML= data.wind.speed+" km/h";
+const searchbtn=document.querySelector(".search button");
+const weathericon=document.querySelector(".weather-icon");
 
-            if(data.weather[0].main=="Clouds"){
-                weathericon.src="images/clouds.png";
-            }
-            else if(data.weather[0].main=="Clear"){
-                weathericon.src="images/clear.png";
-            }
-            else if(data.weather[0].main=="Rain"){
-                weathericon.src="images/rain.png";
-            }
-            else if(data.weather[0].main=="Drizzle"){
-                weathericon.src="images/drizzle.png";
-            }
-            else if(data.weather[0].main=="Mist"){
-                weathericon.src="images/mist.png";
-            }
 
-            document.querySelector(".weather").style.display="block";
-            document.querySelector(".error").style.display="none";
+async function checkWeather(city){
+   
+    
+const response=await fetch(apiurl +city + `&appid=${apikey}`);
+if(response.status== 404){
+    document.querySelector(".error").style.display="block";
+    document.querySelector(".weather").style.display="none";
 
-            }
-        }
+}
+else{
+    var data=await response.json();
+    console.log(data);
 
-        searchbtn.addEventListener("click", ()=>{
-            checkWeather(searchbox.value);
-        })
+document.querySelector(".city").innerHTML= data.name;
+document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+"°C";
+document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
+document.querySelector(".wind").innerHTML= data.wind.speed+" km/h";
+
+if(data.weather[0].main=="Clouds"){
+    weathericon.src="images/clouds.png";
+}
+else if(data.weather[0].main=="Clear"){
+    weathericon.src="images/clear.png";
+}
+else if(data.weather[0].main=="Rain"){
+    weathericon.src="images/rain.png";
+}
+else if(data.weather[0].main=="Drizzle"){
+    weathericon.src="images/drizzle.png";
+}
+else if(data.weather[0].main=="Mist"){
+    weathericon.src="images/mist.png";
+}
+
+document.querySelector(".weather").style.display="block";
+document.querySelector(".error").style.display="none";
+
+}
+}
+
+searchbtn.addEventListener("click", ()=>{
+checkWeather(searchbox.value);
+})
+
